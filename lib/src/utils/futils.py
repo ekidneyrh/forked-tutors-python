cdd2c72656881862078a78d80f2b22a900095339
Ben <bcapper@redhat.com>
2021-10-07 16:08:11 +0100

    Updated futils

--- lib/src/utils/futils.py
+++ lib/src/utils/futils.py
@@ -1,9 +1,23 @@
+from genericpath import exists
+import shutil as sh
 import os
 import logging
 import yaml
 
 def __init__(self):
     pass
+
+def writeFile(folder,filename,contents):
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+    return os.write(folder + '/' + filename, contents)
+
+def readFile(path1):
+    if os.path.exists(path1):
+        array = os.read(path1).split('\n')
+        return array[0].replace('\r', '')
+    else:
+        logging.warning('Unable to locate ' + path1)
 
 def getImageFile(name):
     validImageTypes = ['png','jpg','jpeg','gif']
@@ -15,6 +29,30 @@ def getImageFile(name):
 
 def getParentFolder():
     return os.path.basename(os.path.dirname(os.getcwd)) 
+
+def getDirectories(srcpath):
+    #TODO
+    pass
+
+def verifyFolder(folder):
+    if not os.path.exists(folder):
+        os.mkdir('-p', folder)
+
+def copyFileToFolder(src, dest):
+    if os.path.exists(src):
+        os.mkdir('-p', dest)
+        sh.copy('-rf', src, dest)
+
+def copyFolder(src, dest):
+    os.mkdir('-p', dest)
+    sh.copy('-rf', src, dest)
+
+def readWholeFile(path1):
+    if os.path.exists(path1):
+        array = os.open(path1)
+        return array
+    else:
+        logging.warning('Unable to locate ' + path1)
 
 def readYaml(path):
     yamldata = ''
@@ -29,12 +67,51 @@ def readYaml(path):
         logging.warning('Review this file and try again....')
     return yamldata
 
-def readWholeFile(path):
-    if os.path.exists(path):
-        array = os.open(path)
-        return array
+def readEnrollment(path):
+    yamldata = ''
+    try:
+        yamldata = yaml.load(os.open(path, encoding='utf-8'))
+    except:
+        logging.warning('Tutors ${version} encountered an error reading the enrollment file:')
+        logging.warning('--------------------------------------------------------------')
+        #logging.warning(err.mark.buffer)
+        logging.warning('--------------------------------------------------------------')
+        #logging.warning(err.message)
+        logging.warning('Ignoring enrolling file for the moment....')
+    return yamldata
+
+def readCalendar(path):
+    yamldata = ''
+    try:
+        yamldata = yaml.load(os.open(path, encoding='utf-8'))
+    except:
+        logging.warning('Tutors ${version} encountered an error reading the calendar file:')
+        logging.warning('--------------------------------------------------------------')
+        #logging.warning(err.mark.buffer)
+        logging.warning('--------------------------------------------------------------')
+        #logging.warning(err.message)
+        logging.warning('Ignoring calendar file for the moment....')
+    return yamldata
+
+
+def getHeader(fileName):
+    header = ''
+    array = os.read(fileName).split('\n')
+    if array[0][0] == '#':
+        header = array[0][0:2]
     else:
-        logging.warning('Unable to locate ' + path)
+        header = array[0]
+    return header
+
+def withoutHeader(fileName):
+    content = os.read(fileName)
+    line1 = content.index('\n')
+    content = content[line1 + 1 : len(content)]    
+    content = content.strip()
+    line2 = content.index('\n')
+    if(line2 > -1):
+        content = content[0 : line2]
+    return content
 
 def getHeaderFromBody(body):
     array = body.split('\n')
@@ -48,9 +125,9 @@ def getHeaderFromBody(body):
 def withoutHeaderFromBody(body):
     content = body
     line1 = content.index('\n')
-    content = content[line1 + 1 : content.len()]
+    content = content[line1 + 1 : len(content)]
     content = content.strip()
     line2 = content.index('\n')
     if line2 > -1:
         content = content[0:line2]
-    return content    
+    return content
