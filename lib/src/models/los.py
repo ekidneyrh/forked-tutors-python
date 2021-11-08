8b4ac667ce4e794feea2d94c289b59d309060d19
Ben <BenCapper19@gmail.com>
2021-11-08 15:40:31 +0000

    Finish los model
    Fix #17
    
    Signed-off-by: Ben Capper <BenCapper19@gmail.com>

--- lib/src/models/los.py
+++ lib/src/models/los.py
@@ -4,23 +4,41 @@ from abc import ABC, abstractmethod
 from lib.src.models.lo import LearningObject
 import utils.futils as futils
 import utils.loutils as loutils
-import lo
 
 class DiscreteLearningObject(LearningObject):
-    def __init__(self, parent):
-        super().__init__(parent)
+    def __init__(self):
+        super().__init__()
 
     def reap(self, pattern):
-        pass
-
-    def publish(path):
-        pass
+        self.link = 'error: missing ' + self.lotype
+        resourceList = glob.glob(pattern).sort
+        if len(resourceList) > 0:
+            resourceName = os.path.split(resourceList[0]['name'])
+            self.reap(resourceName)
+            self.link = resourceList[0]
+        else:
+            resourceList = glob.glob('*.md').sort
+            resourceName = os.path.split(resourceList[0]['name'])
+            self.reap(resourceName)
+            self.link = resourceList[0]
+    def publish(self, path):
+        loutils.copyResource(self.folder, path)
 
 class Talk(DiscreteLearningObject):
-    pass
+    def __init__(self):
+        super().__init__()
+        self.lotype = 'talk'
+        self.reap('*.pdf')
+
 
 class PanelTalk(DiscreteLearningObject):
-    pass
+    def __init__(self):
+        super().__init__()
+        self.lotype = 'paneltalk'
+        self.reap('*.pdf')
 
 class Archive(DiscreteLearningObject):
-    pass
+    def __init__(self):
+        super().__init__()
+        self.lotype = 'archive'
+        self.reap('*.zip')
